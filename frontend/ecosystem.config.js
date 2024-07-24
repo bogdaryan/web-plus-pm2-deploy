ab12b0032e44d3df747dee33c8ce14447783a766
Богдан <bogdan10050@gmail.com>
2024-07-24 16:17:24 +0300

    .

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      "post-deploy": "cd frontend && yarn install && yarn build",
+      "post-deploy": "cd frontend && npm install && npm run build",
     },
   },
 };
